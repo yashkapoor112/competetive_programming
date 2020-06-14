d97e010570f739b2129c432317c388f677beed8d
unknown <yashkapoor112@gmai.com>
2020-06-14 11:21:41 +0530

    Add: Larget divisible subset

---
+++ Problems/Dynamic Programming/larget_divisible_subset.cpp
@@ -0,0 +1,37 @@
+/*
+Source code by Yash Kapoor
+
+Codeforces      yk112
+Leetcode        yk112
+Codechef        yashkapoor112
+
+Problem Statement - 
+
+Problem Link - https://leetcode.com/problems/largest-divisible-subset/
+
+*/
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        if(!nums.size())    return {};
+        sort(nums.begin(), nums.end());
+        vector<vector<int> > dpp(nums.size());
+        for(int i = 0; i < dpp.size(); i++)     dpp[i] = {nums[i]};
+        vector<int> anss = dpp[0];
+        int idx = 0, ans = 1;
+        for(int i = 1; i < nums.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i]%nums[j] == 0 || nums[j]%nums[i] == 0){
+                    if(dpp[i].size() < dpp[j].size()+1){
+                        dpp[i] = dpp[j];
+                        dpp[i].push_back(nums[i]);
+                    }
+                    if(dpp[i].size() > anss.size())
+                        anss = dpp[i];
+                }
+            }   
+        }
+        return anss;
+    }
+};
